6ec22b00087ecc40e37befe4aa018c57756323dc
Váczi Szabolcs <vaczi.szabolcs@ckik.loc>
2025-09-19 11:23:44 +0200

    ef valtozok

--- orai.py
+++ orai.py
@@ -4,11 +4,8 @@ a = 13
 b = 24
 c = 36
 d = 48
-<<<<<<< HEAD
 f = 12
-=======
 e = "szöveg"
->>>>>>> ff00f7eae72650565994ae3b07b2033ccd15f27c
 print("a+b", a+b)
 print("a+c", a+c)
 print("b+c", b+c)
